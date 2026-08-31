cd77ed95c6b79bfcce4ba6f65ae973106161ffa3
Arjun <arjunadps662@gmail.com>
2026-08-31 23:06:23 +0530

    Setup application structure and enable Swagger

--- QueueLessAPI/API/Program.cs
+++ QueueLessAPI/API/Program.cs
@@ -1,21 +1,23 @@
+using API.Extensions;
+using Application;
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
+app.UseSwaggerServices();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- QueueLessAPI/Infrastructure/DependencyInjection.cs
+++ QueueLessAPI/Infrastructure/DependencyInjection.cs
@@ -1,6 +1,14 @@
-﻿namespace Infrastructure
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfigurationManager configuration)
+        {
+
+            return services;
+        }
     }
 }
