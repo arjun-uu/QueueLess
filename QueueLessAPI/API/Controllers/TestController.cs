d0aaed1aceb02b6d18267be70e0fd06fc9e69192
Arjun Kumar <arjunadps662@gmail.com>
2026-09-01 22:16:51 +0530

    Remove SendEmail method from TestController
    Removed SendEmail action from TestController.

--- QueueLessAPI/API/Controllers/TestController.cs
+++ QueueLessAPI/API/Controllers/TestController.cs
@@ -19,26 +19,4 @@ public class TestController(ILogger<TestController> logger, IEmailService emailS
 
         return Ok(output);
     }
-
-
-    [HttpPost("send")]
-    public async Task<IActionResult> SendEmail(
-        [FromQuery] string email)
-    {
-        await emailService.SendEmailAsync(
-            email,
-            "Test Email - Queueless",
-            """
-            <h1>Hello from Queueless! 👋</h1>
-
-            <p>This is a test email sent from your ASP.NET Core Web API.</p>
-
-            <p>If you're reading this, your email service is working.</p>
-            """);
-
-        return Ok(new
-        {
-            message = "Email sent successfully"
-        });
-    }
 }
